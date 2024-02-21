a313e9df59a9e9c4a83c3a24b84175f10b370bd4
sagariaa <sagarksingh03@gmail.com>
2024-02-21 20:15:47 +0530

    merge sort added

--- script2.js
+++ script2.js
@@ -9,7 +9,7 @@ function display(data) {
     visualizer.innerHTML = '';
     for (var i = 0; i < data.length; i++) {
         var bar = document.createElement('div');
-        bar.style.height = data[i] + 'px';
+        bar.style.height = data[i]*10 + 'px';
         bar.classList.add('bar');
         visualizer.appendChild(bar);
     }
@@ -20,32 +20,57 @@ function displaypass(data) {
     // sorting.innerHTML = '';
     for (var i = 0; i < data.length; i++) {
         var bar = document.createElement('div');
-        bar.style.height = data[i] + 'px';
+        bar.style.height = data[i]*10 + 'px';
         bar.classList.add('bar');
         sorting.appendChild(bar);
     }
 }
 
+ function mergeSort(data) {
+    if (data.length < 2) {
+        return data;
+    }
+    var mid = Math.floor(data.length / 2);
+    var subLeft = mergeSort(data.slice(0,mid));
+    var subRight = mergeSort(data.slice(mid));
 
+    return merge(subLeft, subRight);
+    
+}
 
-
-async function bubblesort(data) {
-    for(let i = data.length; i >= 1; i--) {
-        for(let j = 1; j <= i; j++) {
-            if(data[j-1] > data[j]) {
-                let temp = data[j-1];
-                data[j-1] = data[j];
-                data[j] = temp;
-            }
-        }
-        console.log(data);
+async function merge(node1, node2) {
+    var result = [];
+    while (node1.length > 0 && node2.length > 0)
+        result.push(node1[0] < node2[0]? node1.shift() : node2.shift());
+    result.push(...node1, ...node2);
+    console.log(data);
         display(data);
         await new Promise(resolve => setTimeout(resolve, 100)); // delay of 100ms
         displaypass(data);
         await new Promise(resolve => setTimeout(resolve, 100));
-    }
 }
 
 document.getElementById('sort').addEventListener('click', async function() {
-    await bubblesort(data);
+    await mergeSort(data);
 });
+
+
+
+// async function bubblesort(data) {
+//     for(let i = data.length; i >= 1; i--) {
+//         for(let j = 1; j <= i; j++) {
+//             if(data[j-1] > data[j]) {
+//                 let temp = data[j-1];
+//                 data[j-1] = data[j];
+//                 data[j] = temp;
+//             }
+//         }
+//         console.log(data);
+//         display(data);
+//         await new Promise(resolve => setTimeout(resolve, 100)); // delay of 100ms
+//         displaypass(data);
+//         await new Promise(resolve => setTimeout(resolve, 100));
+//     }
+// }
+
+
